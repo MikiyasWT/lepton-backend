ecec3cc9af3d15669b41d990ef2aab836c394748
MikiyasWT <mikiywendu44@gmail.com>
2024-08-07 19:25:38 +0300

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Allow all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Allowed HTTP methods
+    allowedHeaders: 'Content-Type, Authorization', // Allowed headers
+  });
   await app.listen(5000);
 }
 bootstrap();
